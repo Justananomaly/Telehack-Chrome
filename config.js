5e20899660a7b9bb73650b77555cc4b1e3d4e5e4
Justananomaly <justananomaly@gmail.com>
2015-09-03 16:32:31 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+window.config = {
+   "model_": "AppConfig",
+   "id": 1,
+   "appName": "Telehack",
+   "version": "1.1",
+   "homepage": "http://www.telehack.com",
+   "enableNavBttns": false,
+   "enableHomeBttn": false,
+   "enableReloadBttn": false,
+   "enableLogoutBttn": false,
+   "kioskEnabled": true,
+};
